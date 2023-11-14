fc7d032453d1203c4e8fec1ec2278ae4aaaaa21a
aasemgar7i <aasemgar7i@gamil.com>
2023-11-15 00:23:52 +0200

    hotfix betty

--- _putchar.c
+++ _putchar.c
@@ -1,19 +1,20 @@
 #include "shell.h"
 
-/*
-* _putchar - main func
-* @c: input
-*/
+/**
+ * _putchar - main func
+ * @c: input
+ * Return: char
+ */
 
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
 
-/*
-* _putchars - main func
-* @str: input
-*/
+/**
+ * _putchars - main func
+ * @str: input
+ */
 
 void _putchars(const char *str)
 {
